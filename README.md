f86405cdfa4e5f525bcacb9add75f48073fbb08d
marco <marco@badshark.io>
2016-11-24 19:00:01 +0100

    First commit.

---
+++ README.md
@@ -0,0 +1,34 @@
+# Rump.
+
+Sync two Redis servers using dumps.
+
+- Sync EC cluster DB 1 to local DB 1.
+`rump -from redis://1234.cache.amazonaws.com:6379/1 -to redis://127.0.0.1:6379/1`
+
+## Why.
+
+[ElastiCache]( http://docs.aws.amazon.com/AmazonElastiCache/latest/UserGuide/ClientConfig.RestrictedCommands.html ) doesn't support any of the standard Redis backup commands.
+
+Rump is able to transfer keys from an ElastiCache cluster or any Redis server to another Redis server, by only using `SCAN`, `DUMP` and `RESTORE`.
+
+## Features.
+
+- Uses `SCAN` instead of `KEYS` to avoid DoS your own server.
+- Can sync any key type.
+- Drops the TTL on purpose, since it wouldn't be in sync.
+- Doesn't use any temp file.
+- Uses buffered channels to optimize slow source servers.
+- Uses pipelines to minimize network roundtrips.
+
+## Examples.
+
+- Sync local Redis DB 1 to DB 2.
+`rump -from redis://127.0.0.1:6379/1 -to redis://127.0.0.1:6379/2`
+
+- Sync ElastiCache cluster to local.
+`rump -from redis://production.cache.amazonaws.com:6379/1 -to redis://127.0.0.1:6379/1`
+
+- Sync protected ElastiCache via EC2 port forwarding.
+`$ ssh -L 6969:production.cache.amazonaws.com:6379 -N ubuntu@xxx.xxx.xxx.xxx &
+rump -from redis://127.0.0.1:6969/1 -to redis://127.0.0.1:6379/1
+`
---
+++ rump.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"flag"
+	"github.com/garyburd/redigo/redis"
+)
+
+// Report all errors to stdout.
+func handle(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+// Scan and queue source keys.
+func get(conn redis.Conn, queue chan<- map[string]string) {
+	var (
+		cursor int64
+		keys []string
+	)
+	batch := make(map[string]string)
+
+	for {
+		// Scan a batch of keys.
+		values, err := redis.Values(conn.Do("SCAN", cursor))
+		handle(err)
+		values, err = redis.Scan(values, &cursor, &keys)
+		handle(err)
+
+		// Get pipelined dumps.
+		for _, key := range keys {
+			conn.Send("DUMP", key)
+		}
+		dumps, err := redis.Strings(conn.Do(""))
+		handle(err)
+
+		// Build batch map.
+		for i, _ := range keys {
+			batch[keys[i]] = dumps[i]
+		}
+
+		// Last iteration of scan.
+		if cursor == 0 {
+			// queue last batch.
+			select {
+			case queue <- batch:
+			}
+			close(queue)
+			break
+		}
+
+		fmt.Printf(">")
+		// queue current batch.
+		queue <- batch
+	}
+}
+
+// Restore a batch of keys on destination.
+func put(conn redis.Conn, queue <-chan map[string]string) {
+	for batch := range queue {
+		for key, value := range batch {
+			conn.Send("RESTORE", key, "0", value)
+		}
+		_, err := conn.Do("")
+		handle(err)
+
+		fmt.Printf(".")
+	}
+}
+
+func main() {
+	from := flag.String("from", "", "example: redis://127.0.0.1:6379/0")
+	to := flag.String("to", "", "example: redis://127.0.0.1:6379/1")
+	flag.Parse()
+
+	source, err := redis.DialURL(*from)
+	handle(err)
+	destination, err := redis.DialURL(*to)
+	handle(err)
+	defer source.Close()
+	defer destination.Close()
+
+	// Channel where batches of keys will pass.
+	queue := make(chan map[string]string, 100)
+
+	// Scan and send to queue.
+	go get(source, queue)
+
+	// Restore keys as they come into queue.
+	put(destination, queue)
+
+	fmt.Println("Sync done.")
+}
