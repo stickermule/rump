35d7d47c106cba2e54e13fae600602bcc6353957
marco <marco@badshark.io>
2016-12-06 17:34:04 +0100

    Pre-compile Windows and ARM binaries

--- multi-arch.sh
+++ multi-arch.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 VERSION="$1"
 
-GOOS=darwin GOARCH=amd64 go build -o rump-$VERSION-darwin-amd64 rump.go 
-GOOS=linux GOARCH=amd64 go build -o rump-$VERSION-linux-amd64 rump.go 
+rm rump-*
+GOOS=darwin GOARCH=amd64 go build -o rump-$VERSION-darwin-amd64 rump.go
+GOOS=linux GOARCH=amd64 go build -o rump-$VERSION-linux-amd64 rump.go
+GOOS=linux GOARCH=arm go build -o rump-$VERSION-linux-arm rump.go
+GOOS=windows GOARCH=amd64 go build -o rump-$VERSION-windows-amd64 rump.go
