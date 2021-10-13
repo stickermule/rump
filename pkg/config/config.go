0ca8a6cd6a2b81f3f7ee6ecea189d218ef31f1fb
Phan Duc <phan.duc@moneyforward.co.jp>
2021-10-13 15:08:47 +0900

    Support concurrency

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -21,13 +21,15 @@ type Resource struct {
 // Silent disables verbose mode.
 // TTL enables keys TTL sync.
 type Config struct {
-	Source     Resource
-	Target     Resource
-	Silent     bool
-	TTL        bool
-	DefaultTTL int
-	Count      int
-	Pattern    string
+	Source        Resource
+	Target        Resource
+	Silent        bool
+	TTL           bool
+	DefaultTTL    int
+	Count         int
+	Pattern       string
+	ReadParallel  int
+	WriteParallel int
 }
 
 // exit will exit and print the usage.
@@ -64,14 +66,16 @@ func getRedisOptions(conn string) (bool, string, string) {
 
 // validate makes sure from and to are Redis URIs or file paths,
 // and generates the final Config.
-func validate(from, to string, silent, ttl bool, defaultTTL int, count int, pattern string) (Config, error) {
+func validate(from, to string, silent, ttl bool, defaultTTL int, count int, pattern string, readParallel int, writeParallel int) (Config, error) {
 	cfg := Config{
-		Source:     Resource{},
-		Target:     Resource{},
-		Silent:     silent,
-		TTL:        ttl,
-		DefaultTTL: defaultTTL,
-		Count:      count,
+		Source:        Resource{},
+		Target:        Resource{},
+		Silent:        silent,
+		TTL:           ttl,
+		DefaultTTL:    defaultTTL,
+		Count:         count,
+		ReadParallel:  readParallel,
+		WriteParallel: writeParallel,
 	}
 
 	cfg.Source.IsRedis, cfg.Source.URI, cfg.Source.Auth = getRedisOptions(from)
@@ -85,6 +89,10 @@ func validate(from, to string, silent, ttl bool, defaultTTL int, count int, patt
 		return cfg, fmt.Errorf("to is required")
 	case !cfg.Source.IsRedis && !cfg.Target.IsRedis:
 		return cfg, fmt.Errorf("file-only operations not supported")
+	case !cfg.Source.IsRedis && cfg.ReadParallel > 1:
+		return cfg, fmt.Errorf("read-parallel is only supported for redis source")
+	case !cfg.Target.IsRedis && cfg.WriteParallel > 1:
+		return cfg, fmt.Errorf("write-parallel is only supported for redis target")
 	case cfg.TTL && cfg.DefaultTTL != 0:
 		return cfg, fmt.Errorf("default ttl can only set if ttl mode is disabled")
 	}
@@ -102,10 +110,12 @@ func Parse() Config {
 	defaultTTL := flag.Int("default-ttl", 0, "optional, default ttl if ttl sync mode is disabled")
 	count := flag.Int("count", 10, "optional, number of record per seconds")
 	pattern := flag.String("pattern", "*", "optional, pattern to scan")
+	readParallel := flag.Int("read-parallel", 1, "optional, number of worker for read")
+	writeParallel := flag.Int("write-parallel", 1, "optional, number of worker for write")
 
 	flag.Parse()
 
-	cfg, err := validate(*from, *to, *silent, *ttl, *defaultTTL, *count, *pattern)
+	cfg, err := validate(*from, *to, *silent, *ttl, *defaultTTL, *count, *pattern, *readParallel, *writeParallel)
 	if err != nil {
 		// we exit here instead of returning so that we can show
 		// the usage examples in case of an error.
